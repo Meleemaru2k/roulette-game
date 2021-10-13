06ec4b6ad5a1627e143b72d444cceb11801b9e9b
Sebastian Börgers <s.boergers@live.de>
2021-10-14 00:08:14 +0200

    small fix for SW

--- src/main.js
+++ src/main.js
@@ -4,19 +4,19 @@ import "./registerServiceWorker";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.register("/service-worker.js").then(
-      function (registration) {
-        // Registration was successful
-        console.log("ServiceWorker registration successful with scope: ", registration.scope);
-      },
-      function (err) {
-        // registration failed :(
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", function () {
+//     navigator.serviceWorker.register("/service-worker.js").then(
+//       function (registration) {
+//         // Registration was successful
+//         console.log("ServiceWorker registration successful with scope: ", registration.scope);
+//       },
+//       function (err) {
+//         // registration failed :(
+//         console.log("ServiceWorker registration failed: ", err);
+//       }
+//     );
+//   });
+// }
 
 createApp(App).mount("#app");
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -4,7 +4,7 @@ import { register } from "register-service-worker";
 
 if (process.env.NODE_ENV === "production") {
   console.log(process.env.BASE_URL);
-  register(`${process.env.BASE_URL}service-worker.js`, {
+  register(`/service-worker.js`, {
     ready() {
       console.log(
         "App is being served from cache by a service worker.\n" +
